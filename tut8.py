02f9efa527958dd18fc289c5f675bffdfb3ae6de
mrinmoybasak <64681624+mrinmoybasak@users.noreply.github.com>
2021-03-15 13:59:19 +0530

    ver3

--- tut8.py
+++ tut8.py
@@ -1,8 +1,12 @@
-mystr = "Harry is a good boy"
-# print(len(mystr))
-# print(mystr[::-2])
+# mystr = "Harry is a good boy"
+# # print(len(mystr))
+# # print(mystr[::-2])
+#
+# print(mystr.endswith("bdoy"))
+# print(mystr.count("o"))
+# print(mystr.capitalize())
+# print(mystr.replace("is", "are"))
 
-print(mystr.endswith("bdoy"))
-print(mystr.count("o"))
-print(mystr.capitalize())
-print(mystr.replace("is", "are"))
+mystr = "Mrinmoy is a good boy"
+print(len(mystr))
+print(mystr[::-1])
